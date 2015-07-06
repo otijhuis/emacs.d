473003ef250ae72311a748ba247a8f71b5a513cd
Okke Tijhuis <o.tijhuis@gmail.com>
2015-07-06 17:40:50 +0200

    disable flycheck
    Some problems with flycheck. Have to look into that later. For now just
    disabling it.

--- config/syntax-checking-settings.el
+++ config/syntax-checking-settings.el
@@ -1,7 +1,7 @@
-(eval-after-load 'flycheck '(flycheck-clojure-setup))
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;;(eval-after-load 'flycheck '(flycheck-clojure-setup))
+;;(add-hook 'after-init-hook #'global-flycheck-mode)
 
-(eval-after-load 'flycheck
-  '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+;;(eval-after-load 'flycheck
+;;  '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
 
 (provide 'syntax-checking-settings)
