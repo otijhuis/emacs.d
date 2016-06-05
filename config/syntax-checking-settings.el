c8cea73dd7f9f18318c029f43c6092eb5097f6ed
Okke Tijhuis <o.tijhuis@gmail.com>
2016-06-05 18:49:11 +0200

    disable flycheck for clojure

--- config/syntax-checking-settings.el
+++ config/syntax-checking-settings.el
@@ -1,18 +1,17 @@
-(with-eval-after-load "flycheck"
-  (flycheck-clojure-setup)
-  (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages)
-  (add-to-list 'flycheck-checkers 'clojure-cider-eastwood)
-  (flycheck-pos-tip-mode))
-;;(add-hook 'after-init-hook #'global-flycheck-mode)
+;; (with-eval-after-load "flycheck"
+;;   (flycheck-clojure-setup)
+;;   (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages)
+;;   (add-to-list 'flycheck-checkers 'clojure-cider-eastwood)
+;;   (flycheck-pos-tip-mode))
 
-(with-eval-after-load "cider"
-  (defun my-cider-mode-enable-flycheck ()
-    (when (and (s-ends-with-p ".clj" (buffer-file-name))
-               (not (s-ends-with-p "/dev/user.clj" (buffer-file-name))))
-      (flycheck-mode 1)))
+;; (with-eval-after-load "cider"
+;;   (defun my-cider-mode-enable-flycheck ()
+;;     (when (and (s-ends-with-p ".clj" (buffer-file-name))
+;;                (not (s-ends-with-p "/dev/user.clj" (buffer-file-name))))
+;;       (flycheck-mode 1)))
 
-  (add-hook 'cider-mode-hook 'my-cider-mode-enable-flycheck)
-  (add-hook 'cider-mode-hook
-            (lambda () (setq next-error-function #'flycheck-next-error-function))))
+;;   (add-hook 'cider-mode-hook 'my-cider-mode-enable-flycheck)
+;;   (add-hook 'cider-mode-hook
+;;             (lambda () (setq next-error-function #'flycheck-next-error-function))))
 
 (provide 'syntax-checking-settings)
